986bce2544eaa251a203b0cfbd71ebac6355a41a
Noilany Martinez Mora <maymm.zyn@gmail.com>
2026-02-10 17:42:47 -0600

    semana 4, crea, modifica y elimina

--- Semana1/src/main/java/com/tienda/domain/Categoria.java
+++ Semana1/src/main/java/com/tienda/domain/Categoria.java
@@ -10,31 +10,66 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.io.Serializable;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Data
 @Getter
 @Setter
 @Entity
-@Table (name = "categoria")
+@Table(name = "categoria")
 public class Categoria implements Serializable {
-    //se recomienda anadir un seriealVersionIUD
+    
     private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
-    public Integer idCategoria;
+    private Integer idCategoria;
     
     @Column(unique = true, nullable = false, length = 50)
     @NotNull
     @Size(max = 50)
-    public String descripcion;
+    private String descripcion;
     
     @Column(length = 1024)
     @Size(max = 1024)
-    public String rutaImagen;
+    private String rutaImagen;
     
     @Column(name = "activo")
-    public Boolean activo;
+    private Boolean activo;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
+ 
